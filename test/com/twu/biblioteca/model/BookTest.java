b8ac5ca81cafaa9db4dee81724e598873611eb9d
Cecy Geraldo <cecygeraldo@MacBook-Pro-de-Cecy.local>
2020-04-15 18:50:43 -0400

    test of getBookTitleShouldReturnBooksTitle() is passing, we did a mockSetUp function to instance testBook as a new Book class object

--- test/com/twu/biblioteca/model/BookTest.java
+++ test/com/twu/biblioteca/model/BookTest.java
@@ -1,10 +1,20 @@
 package com.twu.biblioteca.model;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BookTest {
 
+    private Book testBook;
+
+    @Before
+    public void mockSetUp() {
+
+        testBook = new Book("Clean Code");
+    }
     @Test public void getBookTitleShouldReturnBooksTitle() {
-        assertEquals("Clean Code", book.getBookTitle());
+        assertEquals("Clean Code", testBook.getBookTitle());
     }
 }
