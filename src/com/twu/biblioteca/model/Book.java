a9dc69d9c4e398652ec2a401eb498c9af501d5cc
Cecy Geraldo <cecygeraldo@MacBook-Pro-de-Cecy.local>
2020-04-15 19:05:00 -0400

    Created the code at Book class to add publishedYear as a parameter, second test is passing

--- src/com/twu/biblioteca/model/Book.java
+++ src/com/twu/biblioteca/model/Book.java
@@ -3,10 +3,13 @@ package com.twu.biblioteca.model;
 public class Book {
     private String title;
     private String authorNames;
+    private int publishedYear;
 
-    public Book(String title, String authorNames) {
+
+    public Book(String title, String authorNames, int publishedYear) {
         this.title = title;
         this.authorNames = authorNames;
+        this.publishedYear = publishedYear;
     }
 
     public String getBookTitle() {
@@ -15,6 +18,10 @@ public class Book {
 
     public String getBookAuthorNames() {
         return authorNames;
+    }
+
+    public int getBookPublishedYear() {
+        return publishedYear;
     }
 
 }
