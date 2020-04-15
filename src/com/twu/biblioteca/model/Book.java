e4ad40f90f7b5320e25e28db503b513b5cf4319d
Cecy Geraldo <cecygeraldo@MacBook-Pro-de-Cecy.local>
2020-04-15 18:45:56 -0400

    Created the code at Book Class Model to add a book title

--- src/com/twu/biblioteca/model/Book.java
+++ src/com/twu/biblioteca/model/Book.java
@@ -1,4 +1,15 @@
 package com.twu.biblioteca.model;
 
 public class Book {
+    private String title;
+
+    public Book(String title) {
+        this.title = title;
+
+    }
+
+    public String getBookTitle() {
+        return title;
+    }
+
 }
