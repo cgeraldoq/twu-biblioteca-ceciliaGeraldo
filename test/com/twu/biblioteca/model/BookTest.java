2cbaf15ea68981deb14f6d8d78cb4cf97827ed46
Cecy Geraldo <cecygeraldo@MacBook-Pro-de-Cecy.local>
2020-04-15 18:53:45 -0400

    First test of get author names is created and failing

--- test/com/twu/biblioteca/model/BookTest.java
+++ test/com/twu/biblioteca/model/BookTest.java
@@ -14,7 +14,12 @@ public class BookTest {
 
         testBook = new Book("Clean Code");
     }
+
     @Test public void getBookTitleShouldReturnBooksTitle() {
         assertEquals("Clean Code", testBook.getBookTitle());
+    }
+
+    @Test public void getBookAuthorNameShouldReturnBooksAuthorNames() {
+        assertEquals(" Robert C. Martin", testBook.getBookAuthorName());
     }
 }
