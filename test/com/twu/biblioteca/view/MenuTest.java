45487da3827c65b8c34b92fd256dc3d0fa56189f
Cecy Geraldo <cecygeraldo@MacBook-Pro-de-Cecy.local>
2020-04-23 12:00:31 -0400

    Added test of Return Book menu option

--- test/com/twu/biblioteca/view/MenuTest.java
+++ test/com/twu/biblioteca/view/MenuTest.java
@@ -2,12 +2,35 @@ package com.twu.biblioteca.view;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class MenuTest {
 
     @Test
     public void shouldSeeListBookOptionAtMenu() {
-        assertEquals("1. List Of Books", Menu.menuOptions());
+        assertThat(Menu.menuOptions(), containsString("1. List Of Books"));
     }
+
+    @Test
+    public void shouldSeeCheckoutBookOptionAtMenu() {
+        assertThat(Menu.menuOptions(), containsString("2. Checkout a Book"));
+    }
+    @Test
+    public void shouldSeeQReturnABookOptionAtMenu() {
+        assertThat(Menu.menuOptions(), containsString("3. Return a Book"));
+    }
+    @Test
+    public void shouldSeeQuitBibliotecaOptionAtMenu() {
+        assertThat(Menu.menuOptions(), containsString("9. Quit Biblioteca"));
+    }
+
+    @Test
+    public void shouldSeeAllMenuOptions() {
+        assertEquals("1. List Of Books \n2. Checkout a Book \n3. Return a Book\n9. Quit Biblioteca \n", Menu.menuOptions());
+    }
+
+
+
 }
